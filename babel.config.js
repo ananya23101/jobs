811ad6f92aa7fabd97d7b8978367feb3f73934f1
ananya23101 <ananya23102001@gmail.com>
2024-01-05 13:09:20 +0530

    fff

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo' , 'module:metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo'],
     plugins: [require.resolve("expo-router/babel"),
     ["module:react-native-dotenv", {
       "moduleName": "@env",
